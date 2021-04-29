6da8b36cc31df6017ea1aca9718bacb8013703c0
Daoud Merchant <merchantdaoud@hotmail.com>
2021-04-29 12:18:19 +0200

    Complete readme, refactor counter function, prettify

--- readme.md
+++ readme.md
@@ -17,8 +17,55 @@ I also want the JS functions to be as reusable as possible, in the hope that the
 
 ## Thoughts after completion
 
+Once again, I'm left with a lingering feeling of dissatisfaction from a mixture of feeling like I've been hitting the wall of my abilities and needing to 'abandon' the project before 'completion' because of the endless layers of polish I could always spend more time on. As such, I'm actually going to include a new section called '[**Were time infinite**](#were-time-infinte)'.
+
+The brief didn't describe this as a portfolio project and its wording was very much telling me to 'get it working', so I've decided to stop when:
+
+- The dropdown menu works (the box slides down and the inner links fade in or out as appropriate)
+- The burger menu works (I didn't add animation purely because, as I understand, mobile performance is a major factor and any animation I could have added would have been costly, see [below](#the-problems))
+- The gallery has full functionality, albeit insufficiently adaptive for mobile use
+
+### The problems
+
+So the course invited me to watch [this](https://www.youtube.com/watch?v=8kK-cA99SA0) video (referencing [this](https://www.html5rocks.com/en/tutorials/speed/high-performance-animations/) article) which **specifically** states that *only* `position`, `scale`, `rotate` and `opacity` are easy enough to process to guarantee that the transition will be smooth (I work on a 2016 ThinkPad with an Intel Celeron, which is excellent for testing when something's processor-intensive enough to stutter(!)), so I settled on using `scaleY` from 0 to 1 to achieve the dropdown effect. The caveat I only discovered after implementation is, of course, that the entire space for the element is reserved in the DOM even when scaled to 0, necessitating a set of empty space below for the gallery to still be visible. As such, everything ended up being positioned absolutely, leading to very complicated mobile formatting and a lot of general overcomplication. Were I to redo the task I guess I would have to implement the `max-height` solution suggested on tutorials, despite the fact that this conflicts with the initial assertion on performant transition properties.
+
+Should this be the case, I could also use `max-width` to animate the sidebar in a similar style, or even accordion down space between main links to make room for sublinks to fade in and accommodate a global function for expanding and fading in / fading out and contracting. These are all things I'd like to revisit at a later point with more skill.
+
+As I wanted all of the dropdown links to share the same container below the top nav bar, it meant completely altering the semantic structure of the HTML. I have no idea if this was 'correct', but I tried to keep the HTML as semantically clear as possible (e.g. for those using screen-readers with JavaScript disabled) and had each of my menu functions firstly relocate all of the links in to a newly created `<div>` for display purposes. The problem with *this* approach is that the page doesn't reformat from a dropdown to a sidebar if the window is resized, it only renders correctly for the screen size on initial load (otherwise the function would have to be ready to look not only in the `<nav>` element for the links but any created `<div>` from the alternative menu style). However, at this stage I'm not sure if in deployment two individual CSS files would even be loaded depending on screen size.
+
+Another small thing I noticed was that a previous article in the course specifically advised against inline style (I use it here because the value is reliant upon JS calculation), instead suggesting that JS only toggle classes on and off, which in general I've tried to adhere to. One side-effect is that classes cannot invalidate or update styles applied to a specific element through an ID selector in the .css file (for being lower priority), so for now I have used classes as ID selectors for some elements. The alternative would have been to apply inline style, which would be the solution for a future attempt.
+
+## Were time infinite
+
+- Adjust gallery to be correctly adaptive for smaller screens
+- Extend both side and dropdown menus with `max-[width/height]` properties
+- Toggle dropdown header arrows between down and up when accordion is contracted or extended respectively
+- Find solution for screen to render dropdown or sidebar on resize (note: the same page offering both was *not* specified by the exercise brief)
+
 ## Areas for improvement
+
+### Code organisation
+
+Still, I find organising code such a challenge! I *must* find a resource with some simple programs written out in their entirety with model code organisation. As things stand I'm comfortable putting functions and variables in modules, but struggle to divide scope within the module. I've also read that IIFE's are rarely if ever needed, when for now I use them for page loads as I don't know what the industry standard is!
+
+### Higher Order Functions
+
+Related to the above, I'm trying to keep code DRY but I'm sure there is greater DRYness possible. I feel like there's more order in my gallery function than my menu functions. the function `fillOrClosePanel` specifically looks like a mess, but I can't see refactoring it to higher order functions saving lines of code, as each function would just be to apply one line of code.
+
+### Approaching problems
+
+I guess I just need more experience approaching problems. This took me so long to figure out how to go about the sliding image gallery, but it felt partly just a symptom of having never approached this kind of problem before. I'm just a bit intimidated that YouTube tutorials last about 25mins and it took me days to do this incomplete page(!) Hopefully this is normal, I've emailed some companies based near me to ask if anyone from the team would be free to meet up and discuss my questions and progress for half an hour.
 
 ## Lessons learned
 
-###
+### Skewed elements reserve their entire space in the DOM
+
+### Use inline style if a style applied to an ID needs updating in JS
+
+### Avoid absolute or fixed positioning if not appropriate for the entire page layout
+
+i.e. don't use it as a 'fix'.
+
+### Install Prettier locally from the start
+
+I thought it would make more sense to run 'Prettier' at the end. I was wrong.
--- src/index.js
+++ src/index.js
@@ -1,199 +1,219 @@
-const menu = (function() {
+const menu = (function () {
     // global menu functions
-    const createContainer = function(className) {
-        const container = document.createElement("div");
-        container.className = className;
-        return container;
-    }
-    const moveLink = function(container, links) {
-        container.appendChild(links);
-    }
-
-    const displayDropDown = function(siblingBefore, dropdownLinks) {
-    
-        // make dropdown container
-        const container = createContainer("dropdownbox");
-    
-        // Functions shared by click on link and click on window
-        const fadeOutAndClose = function(linkContainer) {
-            if (!container.classList.contains("extendDown")) {
-                return;
-            }
-            linkContainer.classList.remove("fade"); // fade out links
+    const createContainer = function (className) {
+      const container = document.createElement("div");
+      container.className = className;
+      return container;
+    };
+    const moveLink = function (container, links) {
+      container.appendChild(links);
+    };
+  
+    const displayDropDown = function (siblingBefore, dropdownLinks) {
+      // make dropdown container
+      const container = createContainer("dropdownbox");
+  
+      // Functions shared by click on link and click on window
+      const fadeOutAndClose = function (linkContainer) {
+        if (!container.classList.contains("extendDown")) {
+          return;
+        }
+        linkContainer.classList.remove("fade"); // fade out links
+        setTimeout(() => {
+          linkContainer.classList.remove("revealGrid"); // hide links
+          container.classList.remove("extendDown"); // retract panel
+        }, 250);
+      };
+  
+      const clearSelected = function () {
+        dropdownLinks.forEach((link) => link.classList.remove("selected"));
+      };
+  
+      // function for click on dropdown link
+      const fillOrClosePanel = function () {
+        clearSelected();
+        const linkBox = document.querySelector(`#${this.id}links`);
+        const fadeInLinks = function () {
+          linkBox.classList.add("revealGrid");
+          setTimeout(() => linkBox.classList.add("fade"), 50);
+        };
+        if (!container.classList.contains("extendDown")) {
+          // panel was closed
+          this.classList.add("selected");
+          container.classList.add("extendDown");
+          setTimeout(() => fadeInLinks(), 250);
+        } else {
+          // panel open
+          if (linkBox.classList.contains("revealGrid")) {
+            // currently selected
+            fadeOutAndClose(linkBox);
+          } else {
+            // other links displayed
+            this.classList.add("selected");
+            const currentBox = document.querySelector(".revealGrid");
+            currentBox.classList.remove("fade"); // fade out current
             setTimeout(() => {
-                linkContainer.classList.remove("revealGrid"); // hide links
-                container.classList.remove("extendDown"); // retract panel
+              currentBox.classList.remove("revealGrid");
+              fadeInLinks();
             }, 250);
+          }
         }
-    
-        const clearSelected = function() {
-            dropdownLinks.forEach(link => link.classList.remove("selected"));
+      };
+  
+      // fill container, add dropdown link click events
+      dropdownLinks.forEach((link) => {
+        moveLink(container, link.nextElementSibling);
+        link.addEventListener("click", fillOrClosePanel); // function on link click
+      });
+  
+      // insert container
+      siblingBefore.after(container);
+  
+      // function for click on window
+      window.addEventListener("click", (e) => {
+        if (e.target.tagName !== "A") {
+          clearSelected();
+          const currentBox = document.querySelector(".revealGrid");
+          fadeOutAndClose(currentBox);
         }
-    
-        // function for click on dropdown link
-        const fillOrClosePanel = function() {
-            clearSelected();
-            const linkBox = document.querySelector(`#${this.id}links`);
-            const fadeInLinks = function() {
-                linkBox.classList.add("revealGrid");
-                setTimeout(() => linkBox.classList.add("fade"), 50);
-            }
-            if (!container.classList.contains("extendDown")) { // panel was closed
-                this.classList.add("selected");
-                container.classList.add("extendDown");
-                setTimeout(() => fadeInLinks(), 250);
-            } else { // panel open
-                if (linkBox.classList.contains("revealGrid")) { // currently selected
-                    fadeOutAndClose(linkBox);
-                } else { // other links displayed
-                    this.classList.add("selected");
-                    const currentBox = document.querySelector(".revealGrid");
-                    currentBox.classList.remove("fade"); // fade out current
-                    setTimeout(() => {
-                        currentBox.classList.remove("revealGrid");
-                        fadeInLinks();
-                    }, 250);
-                }
-            }
-        }
-        
-        // fill container, add dropdown link click events
-        dropdownLinks.forEach(link => {
-            moveLink(container, link.nextElementSibling);
-            link.addEventListener("click", fillOrClosePanel); // function on link click
-        })
-
-        // insert container
-        siblingBefore.after(container);
-    
-        // function for click on window
-        window.addEventListener("click", e => {
-            if (e.target.tagName !== "A") {
-                clearSelected();
-                const currentBox = document.querySelector(".revealGrid");
-                fadeOutAndClose(currentBox);
-            }
-        });
+      });
     };
-    
-    const displaySideBar = function(siblingBefore, dropdownLinks, links) {
-        
-        // make sidebar container
-        const container = createContainer("sidebar")
-
-        // fill container
-        moveLink(container, links);
-
-        // add dropdown link click events
-        const toggleSublinks = function() {
-            this.nextElementSibling.classList.toggle("revealFlex");
-        }
-        dropdownLinks.forEach(link => {
-            link.addEventListener("click", toggleSublinks);
-        })
-
-        // insert container
-        siblingBefore.after(container);
-        
-        // add reveal menu on click burger
-        const burger = document.querySelector(".burger");
-        const toggleSidebar = function() {
-            dropdownLinks.forEach(link => link.nextElementSibling.classList.remove("revealFlex"));
-            container.classList.toggle("revealFlex");
-        }
-        burger.addEventListener("click", toggleSidebar);
+  
+    const displaySideBar = function (siblingBefore, dropdownLinks, links) {
+      // make sidebar container
+      const container = createContainer("sidebar");
+  
+      // fill container
+      moveLink(container, links);
+  
+      // add dropdown link click events
+      const toggleSublinks = function () {
+        this.nextElementSibling.classList.toggle("revealFlex");
+      };
+      dropdownLinks.forEach((link) => {
+        link.addEventListener("click", toggleSublinks);
+      });
+  
+      // insert container
+      siblingBefore.after(container);
+  
+      // add reveal menu on click burger
+      const burger = document.querySelector(".burger");
+      const toggleSidebar = function () {
+        dropdownLinks.forEach((link) =>
+          link.nextElementSibling.classList.remove("revealFlex")
+        );
+        container.classList.toggle("revealFlex");
+      };
+      burger.addEventListener("click", toggleSidebar);
     };
-
+  
     return {
-        displayDropDown,
-        displaySideBar
-    }
-})();
-
-// gallery function
-
-const slideGallery = function(gallery, galleryNav) {
+      displayDropDown,
+      displaySideBar,
+    };
+  })();
+  
+  // gallery function
+  
+  const slideGallery = function (gallery, galleryNav) {
     const galleryChildren = galleryNav.children;
-
+  
     // extract elements
-    const icons = Array.from(galleryChildren).slice(1, (galleryNav.children.length - 1));
+    const icons = Array.from(galleryChildren).slice(
+      1,
+      galleryNav.children.length - 1
+    );
     const arrowLeft = galleryChildren[0];
     const arrowRight = galleryChildren[galleryChildren.length - 1];
-
-    // subfunctions
-    const toggleIcon = function() {
-        icons[i].classList.toggle("selectIcon");
-    };
-    const toggleAndMove = function() {
-        toggleIcon();
-        gallery.setAttribute("style", `right:${i}00%`)
-    }
-
-    let i = 0;
-
-    // nav functions
-    const galleryForwards = function() {
-        toggleIcon();
+  
+    // counter module
+    const counter = (function () {
+      let i = 0;
+      const moveForwards = function () {
         if (i < icons.length - 1) {
-            i++;
+          i++;
         } else {
-            i = 0;
+          i = 0;
         }
-        toggleAndMove();
-    }
-    const galleryBackwards = function() {
-        toggleIcon();
+      };
+      const moveBackwards = function () {
         if (i > 0) {
-            i--;
+          i--;
         } else {
-            i = (icons.length - 1); // back to end
+          i = icons.length - 1; // back to end
         }
-        toggleAndMove();
-    }
-    const selectGalleryImage = function(icon) {
-        toggleIcon();
-        i = Number(icon.dataset.image);
-        toggleAndMove();
+      };
+      const selectImage = function (num) {
+        i = Number(num);
+      };
+      return {
+        moveForwards,
+        moveBackwards,
+        selectImage,
+        get current() {
+          return i;
+        },
+      };
+    })();
+  
+    // subfunctions
+    const toggleIcon = function () {
+      icons[counter.current].classList.toggle("selectIcon");
     };
-
+    const toggleAndMove = function () {
+      toggleIcon();
+      gallery.setAttribute("style", `right:${counter.current}00%`);
+    };
+    const moveGallery = function (callback) {
+      toggleIcon();
+      callback();
+      toggleAndMove();
+    };
+  
     // scroll on load
-    const cycleGallery = setInterval(galleryForwards, 5000);
-
+    const cycleGallery = setInterval(
+      () => moveGallery(counter.moveForwards),
+      5000
+    );
+  
     // event listeners
     arrowRight.addEventListener("click", () => {
-        clearInterval(cycleGallery);
-        galleryForwards();
+      clearInterval(cycleGallery);
+      moveGallery(counter.moveForwards);
     });
     arrowLeft.addEventListener("click", () => {
-        clearInterval(cycleGallery);
-        galleryBackwards();
+      clearInterval(cycleGallery);
+      moveGallery(counter.moveBackwards);
     });
-    icons.forEach(icon => icon.addEventListener("click", e => {
+    icons.forEach((icon) =>
+      icon.addEventListener("click", (e) => {
         clearInterval(cycleGallery);
-        selectGalleryImage(e.target);
-    }));
-};
-
-// Note: ALL ABOVE could be put on alternative file and imported
-
-(function() {
+        moveGallery(() => counter.selectImage(e.target.dataset.image));
+      })
+    );
+  };
+  
+  // Note: ALL ABOVE could be put on alternative file and imported
+  
+  (function () {
     // select elements
     const nav = document.querySelector("nav");
     const dropLinks = document.querySelectorAll(".dropdown");
     // choose top or side menu
     const width = window.matchMedia("(min-width: 880px");
-    const chooseMenu = function(width) {
-        if (width.matches) {
-            menu.displayDropDown(nav, dropLinks);
-        } else {
-            const mainLinks = document.querySelector("#mainlinks");
-            menu.displaySideBar(nav, dropLinks, mainLinks);
-        }
-    }
+    const chooseMenu = function (width) {
+      if (width.matches) {
+        menu.displayDropDown(nav, dropLinks);
+      } else {
+        const mainLinks = document.querySelector("#mainlinks");
+        menu.displaySideBar(nav, dropLinks, mainLinks);
+      }
+    };
     chooseMenu(width);
-
+  
     // start gallery
     const gallery = document.querySelector("#gallery");
     const galleryNav = document.querySelector("#gallerynav");
     slideGallery(gallery, galleryNav);
-})();
+  })();
